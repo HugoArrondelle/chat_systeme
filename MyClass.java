5be2c230482fe924576d9bee8a4b67d104b9be37
HugoArrondelle <49070677+HugoArrondelle@users.noreply.github.com>
2020-11-15 15:43:55 +0100

    test

---
+++ MyClass.java
@@ -0,0 +1,25 @@
+/*public class MyClass  {
+
+	private int i = 0;
+
+	public void MyClass(){
+		 int i = 17 ;
+
+	}
+	public static void main(String argv[]) {
+		MyClass t = new MyClass();
+		t.MyClass();
+		System.out.println("valeur " + t.i);
+	}
+}
+*/
+
+public class MyClass  {
+
+	static int i;
+
+	public static void main(String argv[]) {
+	
+		System.out.println(i);
+	}
+}
